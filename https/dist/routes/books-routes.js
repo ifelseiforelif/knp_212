7878f68fd720351dcdcf980be1fa2e648de4229f
Natalya Babenko <ifelseiforelif@gmail.com>
2024-10-21 15:43:07 +0300

    deleted dist

--- https/dist/routes/books-routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { books } from "../data/books.js";
-import { BookController } from "../controllers/book-controller.js";
-const booksRouter = Router();
-booksRouter.get("/", (req, res) => {
-    res.status(200).json(books);
-});
-booksRouter.post("/", BookController.createBook, (req, res) => { });
-export default booksRouter;
